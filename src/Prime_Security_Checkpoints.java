213a6fa595a78440dc275a75e97ab0cb9b32854c
Sushma-1706 <damacharlasushma@gmail.com>
2025-09-11 11:38:46 +0530

    Added new file

---
+++ src/Prime_Security_Checkpoints.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        for(int i=n1;i<=n2;i++){
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
+    }
+    public static boolean isPrime(int val) {
+        if(val==2) return true;
+        else if (val == 0 || val == 1||val%2==0) return false;
+        for(int i=3;i*i<=val;i+=2){
+            if(val%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
