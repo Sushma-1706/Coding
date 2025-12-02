acb5df6dad34dbd7678793d37324ba40538f01a3
Sushma-1706 <damacharlasushma@gmail.com>
2025-12-02 12:26:50 +0530

    added new file

---
+++ src/Missing1_Number.java
@@ -0,0 +1,10 @@
+package Coding.src;
+
+import java.util.Scanner;
+
+public class Missing1_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
--- src/Weird_Algorithm.java
+++ src/Weird_Algorithm.java
@@ -6,18 +6,14 @@ public class Weird_Algorithm {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = n;
-        while(true){
-            System.out.println(n);
-            if (n==1) break;
-            if(n%2==0){
-             n=n/2;
-
-        }else{
-                n=n*3+1;
-
+        while (true) {
+            System.out.println(n+" ");
+         if(n==1)break;
+         if (n%2==0) {
+              n=n/2;
+          }else{
+              n=n*3+1;
+          }
             }
         }
-
     }
-}
